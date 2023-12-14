233d0cf98510b7ccb824b3854ac9dfdcc116e4bc
mwizerwa2 <mfrancoisregis2@gmail.com>
2023-12-14 10:44:12 -0800

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main
+{
+	printf("Size of char= %d byte(s)\n ", sizeof(char));
+	printf("Size of an int = %d bytes \n", sizeof(int));
+	printf("size of long int = %d bytes \n", sizeof(long int));
+	printf("Size of long long int = %d bytes \n", sizeof(long long int));
+	printf("Size of float =%d bytes \n", sizeof(float));
+	
+	return(0);
+
+}
